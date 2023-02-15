43315204ab4eccf351e08e7f8fb02b9d886bc2de
marc russell guico <mrussellguico@gmail.com>
2023-02-16 01:18:58 +0800

    shuffling questions

--- web-app/src/pages/quiz.js
+++ web-app/src/pages/quiz.js
@@ -6,9 +6,15 @@ import { BsX } from "react-icons/bs"
 import { useState } from 'react'
 import data from '../data.json'
 
-export const quiz = () => {
+export default function Quiz() {
 
   const results = data.results;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  
+  const btnClickHandler = () => {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestion(nextQuestion);
+  }
 
   return (
     <div className="page page-home bg-image">
@@ -16,27 +22,24 @@ export const quiz = () => {
       <div className="page-category">
         <img src={LOGO} className='logo'></img>
             <div className='flex-row'>
-                <h3>Category: {results.category}</h3>
-                <p>1 out of {results.length}</p>
+                <h3>Category: {results[currentQuestion].category}</h3>
+                <p>{currentQuestion + 1} out of {results.length}</p>
             </div>
       </div>
       <hr></hr>
       <div className="questions">
-        <p>{results.question}</p>
+        <p>{results[currentQuestion].question}</p>
       </div>
       <hr></hr>
-      <div className="btn">
+      <div className="btn" onClick={btnClickHandler}>
         <button className='btn-true'>
-          <BsCheck className='icons'></BsCheck> True
+          <BsCheck className='icons'></BsCheck>{results[currentQuestion].correct_answer}
         </button>
         <button className='btn-false'>
-          <BsX className='icons'></BsX> False
+          <BsX className='icons'></BsX>{results[currentQuestion].incorrect_answers}
         </button>
       </div>
     </div>
   </div>
   )
 }
-
-
-export default quiz

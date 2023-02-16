fa3a5138ab5eab0b71bd4e56f7d9f474e1c3f3e9
marc russell guico <mrussellguico@gmail.com>
2023-02-16 11:16:57 +0800

    results page showing pero di maview questions hmm

--- web-app/src/pages/quiz.js
+++ web-app/src/pages/quiz.js
@@ -10,35 +10,69 @@ export default function Quiz() {
 
   const results = data.results;
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  
-  const btnClickHandler = () => {
-    const nextQuestion = currentQuestion + 1;
-    setCurrentQuestion(nextQuestion);
-  }
+  const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
 
+	const btnOnclickAnswer = (correct_answer) => {
+		if (correct_answer === results[currentQuestion.correct_answer]) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+
+		if (nextQuestion < results.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+
+  const playAgain = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowScore(false);
+  }
+  
   return (
     <div className="page page-home bg-image">
     <div className='container quiz-container'>
-      <div className="page-category">
-        <img src={LOGO} className='logo'></img>
-            <div className='flex-row'>
-                <h3>Category: {results[currentQuestion].category}</h3>
-                <p>{currentQuestion + 1} out of {results.length}</p>
+    {showScore ? (
+				<div className='score-section'>
+            <div className='final-result'>
+              <img src={LOGO} className='logo'></img>
+              <h2>Final Result</h2>
             </div>
-      </div>
-      <hr></hr>
-      <div className="questions">
-        <p>{results[currentQuestion].question}</p>
-      </div>
-      <hr></hr>
-      <div className="btn" onClick={btnClickHandler}>
-        <button className='btn-true'>
-          <BsCheck className='icons'></BsCheck>True
-        </button>
-        <button className='btn-false'>
-          <BsX className='icons'></BsX>False
-        </button>
-      </div>
+            
+					  <h3>{score} / {results.length}</h3>
+            <h4>Your Score</h4>
+            <button onClick={() => playAgain()}>PLAY AGAIN</button>
+				</div>
+			) : ( 
+        <>
+        <div className="page-category">
+          <img src={LOGO} className='logo'></img>
+              <div className='flex-row'>
+                  <h3>Category: {results[currentQuestion].category}</h3>
+                  <p>{currentQuestion + 1} out of {results.length}</p>
+              </div>
+        </div>
+        <hr></hr>
+
+        <div className="questions">
+          <p>{results[currentQuestion].question}</p>
+        </div>
+        
+        <hr></hr>
+        <div className="btn"   onClick={btnOnclickAnswer(results[currentQuestion].correct_answer)}>
+          <button className='btn-true'>
+            <BsCheck className='icons'></BsCheck>True
+          </button>
+          <button className='btn-false'>
+            <BsX className='icons'></BsX>False
+          </button>
+        </div>
+      </>
+      )}
     </div>
   </div>
   )
